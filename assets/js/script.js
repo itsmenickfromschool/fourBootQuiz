36b563f8587c03b7afb616735e651823f99c1308
Nick Burr <nicholasstclairburr@gmail.com>
2023-07-12 14:54:05 -0600

    have a semi functioning thing that deletes my buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-var question = document.querySelector('.question')
-var = document.querySelector('')
-var = document.querySelector('')
-var = document.querySelector('')
-var = document.querySelector('')
-var = document.querySelector('')
+var countDown = document.querySelector('.countDown');
+var startQuiz = document.querySelector('.startQuiz');
+var quizBtn  = document.querySelectorAll('.btn');
+
+console.log(quizBtn)
+
+//Timer Below!!!
+document.addEventListener('click', function() {
+    for (var i = 0; i < quizBtn.length; i++) {
+        console.log(i);
+        var startButton = document.
+       
+        quizBtn[i].setAttribute("style", "display: none;");
+    }});
+    // var quizBtn = document.querySelectorAll('.btn'); 
+    // quizBtn.setAttribute('style', 'color: blue;');
+    // This function will load my quiz */
