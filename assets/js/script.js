b2d4f6048a62b802ece3277249a86c4b0581641f
Nick Burr <nicholasstclairburr@gmail.com>
2023-07-12 18:23:10 -0600

    added array and objects for the questions, as well as a math random. yay

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,71 @@ var quizBtn  = document.querySelectorAll('.answerBtn');
 var quizBox = document.querySelectorAll('.quizBox');
 var startButton = document.querySelector('#start');
 var correctIncorrect = document.querySelector('.correctIncorrect');
+var question = document.querySelector('#question')
+var startingText = 'Hello! This is a Javascript Quiz, press start to begin! You have 60 seconds. If you get a question incorrect you will have 5 seconds removed from the timer.. Good luck!'
+const question1 = {
+    question: 'question',
+    answer1: 'string',
+    answer2:'str',
+    answerCorrect: 'str',
+    answer3: 'str',
+};
+
+const question2 = {
+    question: 'question',
+    answer1: 'string',
+    answer2:'str',
+    answerCorrect: 'str',
+    answer3: 'str',
+};
+
+const question3 = {
+    question: 'question',
+    answer1: 'string',
+    answer2:'str',
+    answerCorrect: 'str',
+    answer3: 'str',
+};
+
+const question4 = {
+    question: 'question',
+    answer1: 'string',
+    answer2:'str',
+    answerCorrect: 'str',
+    answer3: 'str',
+};
+
+const question5 = {
+    question: 'question',
+    answer1: 'string',
+    answer2:'str',
+    answerCorrect: 'str',
+    answer3: 'str',
+};
+
+const question6 = {
+    question: 'question',
+    answer1: 'string',
+    answer2:'str',
+    answerCorrect: 'str',
+    answer3: 'str',
+};
+
+const question7 = {
+    question: 'question',
+    answer1: 'string',
+    answer2:'str',
+    answerCorrect: 'str',
+    answer3: 'str',
+};
+
+var questionArray = [question1,question2,question3,question4,question5,question6,question7];
+
+
+
+
+
+
 
 console.log(quizBtn);
 
@@ -14,7 +79,9 @@ So far I need to get it to work on the load event instead.. Also need to make a 
 window.addEventListener('load', function() {
     for (var i = 0; i < quizBtn.length; i++) {
         quizBtn[i].setAttribute("style", "display: none;");
+        
     }
+    question.textContent = startingText;
     
 });
 
@@ -22,13 +89,14 @@ startButton.addEventListener('click', function(){
     startButton.setAttribute('style', 'display: none;');
 
     for (var i = 0; i < quizBtn.length; i++) {
-        quizBtn[i].setAttribute("style", "display: flex");
-}});
+        quizBtn[i].setAttribute("style", "display: flex")};
+    var questionUsed = Math.floor(Math.random() * questionArray.length);
+    console.log(questionUsed);
+
+});
 
 
 
 
 
-    // var quizBtn = document.querySelectorAll('.btn'); 
-    // quizBtn.setAttribute('style', 'color: blue;');
-    // This function will load my quiz */
+  
